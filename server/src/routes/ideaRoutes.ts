6f2d20886bd92866eae8a231e187c1a88159608f
rpsloup <rpsloup@proton.me>
2023-02-14 18:52:50 +0100

    BE: Adding an endpoint for fetching a single idea

--- server/src/routes/ideaRoutes.ts
+++ server/src/routes/ideaRoutes.ts
@@ -14,4 +14,17 @@ ideaRouter.get('/', async (_, res) => {
   }
 });
 
+ideaRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const idea = await pool.query('SELECT * FROM Ideas WHERE idea_id = $1', [
+      id,
+    ]);
+    return res.json(idea?.rows[0] ?? null);
+  } catch (error) {
+    console.log(error);
+    return res.json(null);
+  }
+});
+
 export default ideaRouter;
