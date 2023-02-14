c07c5d389f3d0da372c2464be7e52e30d0fd0ac1
rpsloup <rpsloup@proton.me>
2023-02-14 19:19:22 +0100

    SQL: Adding a declaration for the Categories table and linking categories in the Ideas table

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,5 +1,15 @@
+CREATE TABLE Categories (
+  category_id SERIAL PRIMARY KEY,
+  name VARCHAR(32),
+  icon VARCHAR(32)
+);
+
 CREATE TABLE Ideas (
   idea_id SERIAL PRIMARY KEY,
+  category_id SERIAL,
   title VARCHAR(72),
-  description TEXT
+  description TEXT,
+  CONSTRAINT category_id
+    FOREIGN KEY (category_id)
+      REFERENCES Categories(category_id)
 );
