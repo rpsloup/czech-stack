63dada0fec04a95a42fc46b26081cc766eeb3b63
rpsloup <rpsloup@proton.me>
2023-03-13 11:39:25 +0100

    BE: Adding and setting up Express

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import { DEFAULT_EXPRESS_PORT } from './utils/constants';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const expressPort = process.env.EXPRESS_PORT || DEFAULT_EXPRESS_PORT;
+app.listen(expressPort, () => {
+  console.log(`[STATUS] Express listening on port ${expressPort}...`);
+});
