a04976bc347628b8f2db1efc85c41191045d7180
rpsloup <rpsloup@proton.me>
2023-03-13 11:50:08 +0100

    BE: Adding and setting up pg

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
-import { DEFAULT_EXPRESS_PORT } from './utils/constants';
+import {
+  DEFAULT_EXPRESS_PORT,
+  DEFAULT_POSTGRES_PORT,
+  DEFAULT_POSTGRES_HOST,
+} from './utils/constants';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
+
+export const pool = new Pool({
+  host: process.env.DB_HOST || DEFAULT_POSTGRES_HOST,
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || '',
+  port: Number(process.env.DB_PORT) || DEFAULT_POSTGRES_PORT,
+});
 
 const expressPort = process.env.EXPRESS_PORT || DEFAULT_EXPRESS_PORT;
 app.listen(expressPort, () => {
